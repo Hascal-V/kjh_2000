83403e6794e1e157e973bf8358bc083856d40dd7
Hascal <hascal0711@gmail.com>
2022-07-07 21:25:14 +0900

    [Update] edit resource
    2022_07

--- app/src/main/java/com/hascal/myapplication/MainActivity.kt
+++ app/src/main/java/com/hascal/myapplication/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.hascal.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Switch
 import android.widget.TextView
+import androidx.appcompat.widget.SwitchCompat
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +17,45 @@ class MainActivity : AppCompatActivity() {
 
         val tvDate = findViewById<TextView>(R.id.tv_date)
 
+        val cardAlert = findViewById<View>(R.id.card_alert)
 
+        val cardCL = cardAlert.findViewById<ConstraintLayout>(R.id.card_cl)
+
+        var swCard = cardCL.findViewById<Switch>(R.id.switchAlert)
+
+        val fabAdd = findViewById<FloatingActionButton>(R.id.fabAdd)
+
+        var timeal = cardCL.findViewById<TextView>(R.id.timeAlert)
+        var timeal2 = cardCL.findViewById<TextView>(R.id.timeAlert2)
+        var dateal = cardCL.findViewById<TextView>(R.id.dateAlert)
+
+        if (swCard.isChecked) {
+            timeal.setTextColor(resources.getColor(R.color.black))
+            timeal2.setTextColor(resources.getColor(R.color.black))
+            dateal.setTextColor(resources.getColor(R.color.black))
+        }
+        else {
+            timeal.setTextColor(resources.getColor(R.color.gray))
+            timeal2.setTextColor(resources.getColor(R.color.gray))
+            dateal.setTextColor(resources.getColor(R.color.gray))
+        }
+
+        swCard.setOnCheckedChangeListener { compoundButton, b ->
+            if (b) {
+                timeal.setTextColor(resources.getColor(R.color.black))
+                timeal2.setTextColor(resources.getColor(R.color.black))
+                dateal.setTextColor(resources.getColor(R.color.black))
+            }
+            else {
+                timeal.setTextColor(resources.getColor(R.color.gray))
+                timeal2.setTextColor(resources.getColor(R.color.gray))
+                dateal.setTextColor(resources.getColor(R.color.gray))
+            }
+        }
+
+        fabAdd.setOnClickListener {
+            val intent = Intent(this, CreateAlertActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
